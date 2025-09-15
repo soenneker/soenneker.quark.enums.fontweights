8a5e36fec4956ae1b982397589512bb78e89f872
Jake Soenneker <jake@soenneker.com>
2025-09-15 16:56:44 -0500

    Initial commit

---
+++ src/FontWeight.cs
@@ -0,0 +1,20 @@
+﻿using Intellenum;
+using Soenneker.Quark.Enums.GlobalKeywords;
+
+namespace Soenneker.Quark.Enums.FontWeights;
+
+/// <summary>
+/// Typed values for the CSS font-weight property in .NET enumeration form.
+/// </summary>
+[Intellenum<string>]
+public sealed partial class FontWeight
+{
+    public static readonly FontWeight Light = new("light");
+    public static readonly FontWeight Normal = new("normal");
+    public static readonly FontWeight Medium = new("medium");
+    public static readonly FontWeight Semibold = new("semibold");
+    public static readonly FontWeight Bold = new("bold");
+    public static readonly FontWeight Bolder = new("bolder");
+
+    public static implicit operator FontWeight(GlobalKeyword style) => new(style.Value);
+}
---
+++ test/Soenneker.Quark.Enums.FontWeights.Tests/FontWeightTests.cs
@@ -0,0 +1,13 @@
+﻿using Soenneker.Tests.Unit;
+using Xunit;
+
+namespace Soenneker.Quark.Enums.FontWeights.Tests;
+
+public sealed class FontWeightTests : UnitTest
+{
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
