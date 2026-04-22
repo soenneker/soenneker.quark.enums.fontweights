a3af7ebd3d3598d4d7d29eb1cb2e0b80be8672f9
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:02:28 -0500

    TUnit conversion

--- test/Soenneker.Quark.Enums.FontWeights.Tests/FontWeightTests.cs
+++ test/Soenneker.Quark.Enums.FontWeights.Tests/FontWeightTests.cs
@@ -1,11 +1,10 @@
 using Soenneker.Tests.Unit;
-using Xunit;
 
 namespace Soenneker.Quark.Enums.FontWeights.Tests;
 
 public sealed class FontWeightTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
