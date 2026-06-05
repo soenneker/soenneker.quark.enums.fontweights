4804a75792303ea42f99cff2640c8824a800e0eb
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:37:15 -0500

    xml commenting

--- src/Soenneker.Quark.Enums.FontWeights/FontWeightKeyword.cs
+++ src/Soenneker.Quark.Enums.FontWeights/FontWeightKeyword.cs
@@ -9,10 +9,28 @@ namespace Soenneker.Quark;
 [IncludeEnumValues(typeof(GlobalKeyword))]
 public sealed partial class FontWeightKeyword
 {
+    /// <summary>
+    /// The lighter.
+    /// </summary>
     public static readonly FontWeightKeyword Lighter = new("lighter");
+    /// <summary>
+    /// The light.
+    /// </summary>
     public static readonly FontWeightKeyword Light = new("light");
+    /// <summary>
+    /// The normal.
+    /// </summary>
     public static readonly FontWeightKeyword Normal = new("normal");
+    /// <summary>
+    /// The semibold.
+    /// </summary>
     public static readonly FontWeightKeyword Semibold = new("semibold");
+    /// <summary>
+    /// The bold.
+    /// </summary>
     public static readonly FontWeightKeyword Bold = new("bold");
+    /// <summary>
+    /// The bolder.
+    /// </summary>
     public static readonly FontWeightKeyword Bolder = new("bolder");
 }
